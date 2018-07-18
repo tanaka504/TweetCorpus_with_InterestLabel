ac91b3349998687a88b3e45444a62493fcac10f6
tanaka504 <tanan2-o-univ.kisasage@outlook.jp>
2018-07-18 14:45:33 +0900

    add tweet crawl script

--- Preprocess.py
+++ Preprocess.py
@@ -1,5 +1,14 @@
-import re
-import sys
+# -*- coding:utf-8 -*-
+
+import re, sys, json, config, time
+from requests_oauthlib import OAuth1Session
+from pprint import pprint
+
+
+CK = config.CONSUMER_KEY
+CS = config.CONSUMER_SECRET
+AT = config.ACCESS_TOKEN
+ATS = config.ACCESS_TOKEN_SECRET
 
 repid = re.compile(r'@[a-zA-Z0-9_]+[\s|\n]')
 url = re.compile(r'http(s)?://[\x20-\x7E]+')
@@ -9,6 +18,8 @@ kaomoji = re.compile \
 haihun = re.compile(r'[-‐‑–—−ｰ]')
 retweet = re.compile(r'RT[a-zA-Z0-9]+')
 qt = re.compile(r'QT')
+
+oath = OAuth1Session(CK, CS, AT, ATS)
 
 def sub_processor(sentence):
     sentence = repid.sub('', sentence)  #リプライのユーザID(@hogehoge)を削除
@@ -24,6 +35,35 @@ def sub_processor(sentence):
 
     return sentence
 
+def get_tweet(id):
+    url = 'https://api.twitter.com/1.1/statuses/show.json'
+    params = {'id': id}
+    res = oath.get(url, params=params)
+    while 1:
+        if res.status_code == 200:
+            timelines = json.loads(res.text)
+            return timelines['text']
+        elif res.status_code == 429:
+            time.sleep(900)
+        else:
+            print('Failed: %d' % res.status_code)
+            print(id)
+            return 'ツイートの取得に失敗しました'
+
+def main():
+    with open('Annotated_Users_Interest.json', 'r') as f:
+        json_data = f.read()
+        data = json.loads(json_data)
+
+        for id, tweet in data.items():
+            sentence = get_tweet(tweet['TweetID'])
+            sentence = sub_processor(sentence)
+            tweet['text'] = sentence
+            data[id] = tweet
+
+    with open('Annotated_Users_Interest_Preprocessed.json', 'w') as out_f:
+        json.dump(data, out_f, ensure_ascii=False, indent=4, separators=(',', ':'))
+
+
 if __name__ == '__main__':
-    sentence = sys.argv[1]
-    print(sub_processor(sentence))
+    main()
